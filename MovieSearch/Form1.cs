4166a4ae98dec16765b39ef452d578504585ade8
Ludwig Dischner <fritz@dischner.me>
2024-01-06 13:33:34 -0800

    feat: add context menu to directory list

--- MovieSearch/Form1.cs
+++ MovieSearch/Form1.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Windows.Forms;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.ComponentModel;
 
 namespace MovieSearch
 {
@@ -22,6 +24,30 @@ namespace MovieSearch
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ClearDirList()
+        {
+            lstDirList.Items.Clear();
+        }
+
+        private void AddDirectory()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string dir = fbd.SelectedPath;
+                    if (dir.Substring(dir.Length - 1) != @"\")
+                    {
+                        dir = dir + @"\";
+                    }
+                    lstDirList.Items.Add(dir);
+                }
+            }
+            lstDirList.Sorted = true;
         }
 
         private string removeFilePath(string sFileName)
@@ -67,7 +93,7 @@ namespace MovieSearch
 
                 lstMovies.DataSource = dtFilteredTable;
             }
-            lblMovies.Text = string.Format("{0} Movies", lstMovies.Items.Count.ToString());
+            lblMovies.Text = string.Format("{0} Result{1}", lstMovies.Items.Count.ToString(), lstMovies.Items.Count == 1 ? "(s)" : "");
         }
 
         private void btnScan_Click(object sender, EventArgs e)
@@ -121,7 +147,7 @@ namespace MovieSearch
                 lstMovies.DataSource = dt;
                 lstMovies.ValueMember = "MovieTitle";
                 lstMovies.DisplayMember = "MovieTitle";
-                lblMovies.Text = string.Format("{0} Result(s)", lstMovies.Items.Count.ToString());
+                lblMovies.Text = string.Format("{0} Result{1}", lstMovies.Items.Count.ToString(), lstMovies.Items.Count == 1 ? "(s)" : "");
                 SortDataTable();
                 MessageBox.Show("Loaded!");
             }
@@ -183,12 +209,46 @@ namespace MovieSearch
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            lstDirList.Items.Clear();
+            ClearDirList();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             EmptyMovies();
         }
+        private void DirList_ContextMenuStrip_Opening(object sender, CancelEventArgs e)
+        {
+            if (lstDirList.SelectedItem == null)
+                e.Cancel = true;
+        }
+
+        private void lstDirList_MouseDown(object sender, MouseEventArgs e)
+        {
+            lstDirList.SelectedIndex = lstDirList.IndexFromPoint(e.X, e.Y);
+        }
+        private void Browse_DirList_MenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", lstDirList.SelectedItem.ToString());
+        }
+
+        private void Remove_DirList_MenuItem_Click(object sender, EventArgs e)
+        {
+            var item = lstDirList.SelectedItem;
+            if (item != null)
+            {
+                lstDirList.Items.Remove(item);
+            }
+        }
+
+        private void Add_DirList_MenuItem_Click(object sender, EventArgs e)
+        {
+            AddDirectory();
+        }
+
+        private void ClearAll_DirList_MenuItem_Click(object sender, EventArgs e)
+        {
+            ClearDirList();
+        }
+
     }
 }
